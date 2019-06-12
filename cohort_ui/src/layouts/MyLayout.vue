7a553df16d9aca71824923e6719ed97336ba1c4f
sonali1117 <sonali.rout@d3analytics.com>
2019-06-12 16:33:28 +0530

    cohort code

---
+++ cohort_ui/src/layouts/MyLayout.vue
@@ -0,0 +1,66 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar class="Top---Menu-Bar pad10LR q-pa-xs">
+        <q-toolbar-title>
+            <div class="headerLeft">
+                <img src="../statics/imgs/logo.png" class="logo">
+            </div>
+
+            <div class="headerRight">
+                <div class="TopMenu">
+                  <span class="Cohorts q-ml-sm">Cohort</span>
+                  <span class="Code-Group q-ml-lg">Code-Group</span>
+                </div>
+
+                <img src="../statics/imgs/Settings.png" class="Setting">
+                <div class="row">
+                  <div class="col">
+                    <img src="../statics/imgs/userIcon.png" class="userIcon">
+                  </div>
+                  <div class="col">
+                    <span class="userName">John Doe</span>
+                  </div>
+                </div>
+            </div>
+        </q-toolbar-title>
+      </q-toolbar>
+      <q-toolbar class="secondary-header row justify-between">
+        <div class="col Cohort-Name">
+        </div>
+        <div class="col">
+          <q-btn class="Rectangle-199 selected q-ml-lg ">
+            <label class="sub-level-menus-labels">Cohort Definition</label>
+          </q-btn>
+          <q-btn class="Rectangle-199 q-ml-lg">
+            <label class="sub-level-menus-labels">Summary</label>
+          </q-btn>
+        </div>
+        <div class="col">
+        </div>
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { openURL } from 'quasar'
+
+export default {
+  name: 'MyLayout',
+  data () {
+    return {
+      leftDrawerOpen: this.$q.platform.is.desktop
+    }
+  },
+  methods: {
+    openURL
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ cohort_ui/src/pages/createCohort.vue
@@ -0,0 +1,71 @@
+<template>
+  <q-page class="app-layout">
+    <div class="row q-pa-lg">
+        <q-card class="row col-8">
+            <div class="col q-ma-sm">
+                <q-input filled v-model="cname" placeholder="Cohort Name" />
+            </div>
+            <div class="col q-ma-sm">
+                <q-input filled v-model="cdesc" placeholder="Cohort Description" />
+            </div>
+            <div class="col q-ma-sm">
+                <q-select filled v-model="cgrp" :options="cGrpOpts" label="Cohort Group"/>
+            </div>
+            <div class="col q-ma-sm">
+                <q-select filled v-model="cdtsource" :options="dtSourceOpts" label="Data Source"/>
+            </div>
+        </q-card>
+        <q-card class="col-3 q-ma-sm q-pa-sm">
+            <q-btn icon="img:statics/imgs/save.png" label="Save"/>
+            <q-btn icon="img:statics/imgs/play.png" label="Run"/>
+            <q-btn icon="img:statics/imgs/delete.png"/>
+        </q-card>
+    </div>
+    <q-card class="row">
+      <criteria-set class="col-2 q-pa-lg"></criteria-set>
+      <criteria-set-details class="col-10 q-pa-lg" :cname="cname" :cdesc="cdesc"></criteria-set-details>
+    </q-card>
+   </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+import criteriaSet from 'pages/criteriaSet'
+import criteriaSetDetails from 'pages/criteriaSetDetails'
+import {
+  QInput,
+  QCard,
+  QSelect
+} from 'quasar'
+export default {
+  name: 'createCohort',
+  components: {
+    QInput,
+    QCard,
+    QSelect,
+    'criteria-set': criteriaSet,
+    'criteria-set-details': criteriaSetDetails
+  },
+  data () {
+    return {
+      cname: '',
+      cdesc: '',
+      cgrp: '',
+      cdtsource: '',
+      scannedAisle: '',
+      cGrpOpts: [
+        { 'label': 'GRP1', 'value': 'GRP1' },
+        { 'label': 'GRP2', 'value': 'GRP2' },
+        { 'label': 'GRP3', 'value': 'GRP3' }
+      ],
+      dtSourceOpts: [
+        { 'label': 'dt1', 'value': 'dt1' },
+        { 'label': 'dt2', 'value': 'dt2' },
+        { 'label': 'dt3', 'value': 'dt3' }
+      ]
+    }
+  }
+}
+</script>
