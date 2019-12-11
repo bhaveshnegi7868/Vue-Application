1ed1b58e5bebe0ec670b47e8ea6e31bc65137a77
Bhavesh Negi <bhavesh.negi@dcube.infra>
2019-12-11 15:49:27 +0530

    conflicts resolved

--- quasar.conf.js
+++ quasar.conf.js
@@ -95,7 +95,7 @@ module.exports = function (ctx) {
 
     devServer: {
       // https: true,
-      // port: 8090,
+      // port: 8080,
       open: true // opens browser window automatically
     },
 
